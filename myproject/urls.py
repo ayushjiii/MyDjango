eba354e05448cb8ae6fc4742b9e30d08c8dbe57c
Ayush Ji <ayushbeladiya65@gmail.com>
2026-04-24 12:36:31 +0530

    basic_auth

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from apod.views import history, home, detail, toggle_favourite,favourites
 from django.contrib import admin
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
+    path('accounts/',include('django.contrib.auth.urls')),
     path('history/', history, name="history"),
     path('apod/<int:id>/', detail, name = "detail"),
     path('favourite/<int:id>/', toggle_favourite, name = "toggle_favourite"),
     path('favourites/',favourites, name = "favourites")
-]
+]  
